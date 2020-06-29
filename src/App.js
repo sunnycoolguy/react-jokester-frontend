e52211325f413418dc0af99fb00c5b890208dd05
senaybee <senay_berihu@outlook.com>
2020-06-29 14:24:52 -0700

    Finish styling navbar

--- src/App.js
+++ src/App.js
@@ -5,84 +5,84 @@ class App extends React.Component{
     return (
       <div>
         <div className='nav'>
-          <ul>
-            <li className='title'><a>JOKERY</a></li>
-            <li><a>All Jokes</a></li>
-            <li><a>My Jokes</a></li>
-            <li><a>Write a Joke</a></li>
-            <li className='sign-out'><a>Sign out</a></li>
-          </ul>
+          <a id='title'>JOKERY</a>
+          <a>Sign Out</a>
+          <a>Write a Joke</a>
+          <a>My Jokes</a>
+          <a>All Jokes</a>
         </div>
-        <div className='master-card first'>
-          <div className='joke-stats'>
-            <div>1230</div>
-            <div id='hah'>HAH!</div>
-            <div id='nah'>nah.</div>
-            <div>43</div>
+        <div className='main'>
+          <div className='master-card first'>
+            <div className='joke-stats'>
+              <div>1230</div>
+              <div id='hah'>HAH!</div>
+              <div id='nah'>nah.</div>
+              <div>43</div>
+            </div>
+            <div className='joke'>
+              <p>Setup : ________</p>
+              <p>Punchline: _______</p>
+            </div>
           </div>
-          <div className='joke'>
-            <p>Setup : ________</p>
-            <p>Punchline: _______</p>
+          <div className='master-card'>
+            <div className='joke-stats'>
+              <div>1230</div>
+              <div id='hah'>HAH!</div>
+              <div id='nah'>nah.</div>
+              <div>43</div>
+            </div>
+            <div className='joke'>
+              <p>Setup : ________</p>
+              <p>Punchline: _______</p>
+            </div>
           </div>
-        </div>
-        <div className='master-card'>
-          <div className='joke-stats'>
-            <div>1230</div>
-            <div id='hah'>HAH!</div>
-            <div id='nah'>nah.</div>
-            <div>43</div>
+          <div className='master-card'>
+            <div className='joke-stats'>
+              <div>1230</div>
+              <div id='hah'>HAH!</div>
+              <div id='nah'>nah.</div>
+              <div>43</div>
+            </div>
+            <div className='joke'>
+              <p>Setup : ________</p>
+              <p>Punchline: _______</p>
+            </div>
           </div>
-          <div className='joke'>
-            <p>Setup : ________</p>
-            <p>Punchline: _______</p>
+          <div className='master-card'>
+            <div className='joke-stats'>
+              <div>1230</div>
+              <div id='hah'>HAH!</div>
+              <div id='nah'>nah.</div>
+              <div>43</div>
+            </div>
+            <div className='joke'>
+              <p>Setup : ________</p>
+              <p>Punchline: _______</p>
+            </div>
           </div>
-        </div>
-        <div className='master-card'>
-          <div className='joke-stats'>
-            <div>1230</div>
-            <div id='hah'>HAH!</div>
-            <div id='nah'>nah.</div>
-            <div>43</div>
+          <div className='master-card'>
+            <div className='joke-stats'>
+              <div>1230</div>
+              <div id='hah'>HAH!</div>
+              <div id='nah'>nah.</div>
+              <div>43</div>
+            </div>
+            <div className='joke'>
+              <p>Setup : ________</p>
+              <p>Punchline: _______</p>
+            </div>
           </div>
-          <div className='joke'>
-            <p>Setup : ________</p>
-            <p>Punchline: _______</p>
-          </div>
-        </div>
-        <div className='master-card'>
-          <div className='joke-stats'>
-            <div>1230</div>
-            <div id='hah'>HAH!</div>
-            <div id='nah'>nah.</div>
-            <div>43</div>
-          </div>
-          <div className='joke'>
-            <p>Setup : ________</p>
-            <p>Punchline: _______</p>
-          </div>
-        </div>
-        <div className='master-card'>
-          <div className='joke-stats'>
-            <div>1230</div>
-            <div id='hah'>HAH!</div>
-            <div id='nah'>nah.</div>
-            <div>43</div>
-          </div>
-          <div className='joke'>
-            <p>Setup : ________</p>
-            <p>Punchline: _______</p>
-          </div>
-        </div>
-        <div className='master-card last'>
-          <div className='joke-stats'>
-            <div>1230</div>
-            <div id='hah'>HAH!</div>
-            <div id='nah'>nah.</div>
-            <div>43</div>
-          </div>
-          <div className='joke'>
-            <p>Setup : ________</p>
-            <p>Punchline: _______</p>
+          <div className='master-card last'>
+            <div className='joke-stats'>
+              <div>1230</div>
+              <div id='hah'>HAH!</div>
+              <div id='nah'>nah.</div>
+              <div>43</div>
+            </div>
+            <div className='joke'>
+              <p>Setup : ________</p>
+              <p>Punchline: _______</p>
+            </div>
           </div>
         </div>
       </div>
