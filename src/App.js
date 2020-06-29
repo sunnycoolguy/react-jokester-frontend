0f9159ac9529d36f3b20a0ce08885889f16f83c7
senaybee <senay_berihu@outlook.com>
2020-06-29 13:07:14 -0700

    Set up main styling for web site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <div className='nav'>
+          <ul>
+            <li className='title'><a>JOKERY</a></li>
+            <li><a>All Jokes</a></li>
+            <li><a>My Jokes</a></li>
+            <li><a>Write a Joke</a></li>
+            <li className='sign-out'><a>Sign out</a></li>
+          </ul>
+        </div>
+        <div className='master-card first'>
+          <div className='joke-stats'>
+            <div>1230</div>
+            <div id='hah'>HAH!</div>
+            <div id='nah'>nah.</div>
+            <div>43</div>
+          </div>
+          <div className='joke'>
+            <p>Setup : ________</p>
+            <p>Punchline: _______</p>
+          </div>
+        </div>
+        <div className='master-card'>
+          <div className='joke-stats'>
+            <div>1230</div>
+            <div id='hah'>HAH!</div>
+            <div id='nah'>nah.</div>
+            <div>43</div>
+          </div>
+          <div className='joke'>
+            <p>Setup : ________</p>
+            <p>Punchline: _______</p>
+          </div>
+        </div>
+        <div className='master-card'>
+          <div className='joke-stats'>
+            <div>1230</div>
+            <div id='hah'>HAH!</div>
+            <div id='nah'>nah.</div>
+            <div>43</div>
+          </div>
+          <div className='joke'>
+            <p>Setup : ________</p>
+            <p>Punchline: _______</p>
+          </div>
+        </div>
+        <div className='master-card'>
+          <div className='joke-stats'>
+            <div>1230</div>
+            <div id='hah'>HAH!</div>
+            <div id='nah'>nah.</div>
+            <div>43</div>
+          </div>
+          <div className='joke'>
+            <p>Setup : ________</p>
+            <p>Punchline: _______</p>
+          </div>
+        </div>
+        <div className='master-card'>
+          <div className='joke-stats'>
+            <div>1230</div>
+            <div id='hah'>HAH!</div>
+            <div id='nah'>nah.</div>
+            <div>43</div>
+          </div>
+          <div className='joke'>
+            <p>Setup : ________</p>
+            <p>Punchline: _______</p>
+          </div>
+        </div>
+        <div className='master-card last'>
+          <div className='joke-stats'>
+            <div>1230</div>
+            <div id='hah'>HAH!</div>
+            <div id='nah'>nah.</div>
+            <div>43</div>
+          </div>
+          <div className='joke'>
+            <p>Setup : ________</p>
+            <p>Punchline: _______</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
