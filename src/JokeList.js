e98fec3a0e9668f98e9f51b6590ebb29427965a4
senaybee <senay_berihu@outlook.com>
2020-07-04 16:36:54 -0700

    Extract JokeCard component

--- src/JokeList.js
+++ src/JokeList.js
@@ -3,19 +3,7 @@ import React from 'react';
 const JokeList = () => {
     return (
         <div className='main'>
-          <div className='master-card first'>
-            <div className='joke-stats'>
-              <div>1230</div>
-              <div id='hah'>HAH!</div>
-              <div id='nah'>nah.</div>
-              <div>43</div>
-            </div>
-            <div className='joke'>
-              <p>Posted by: ___</p>
-              <p>Setup : ________</p>
-              <p>Punchline: _______</p>
-            </div>
-          </div>
+
           <div className='master-card'>
             <div className='joke-stats'>
               <div>1230</div>
