d9859701208b6b9ba1948c48cdd1c6a3c153db81
senaybee <senay_berihu@outlook.com>
2020-07-04 16:56:11 -0700

    Refactor JokeList to fetch data from backend

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,71 +1,44 @@
 import React from 'react';
+import JokeCard from './JokeCard.js';
+import { isCompositeComponent } from 'react-dom/test-utils';
 
-const JokeList = () => {
-    return (
+class JokeList extends React.Component {
+    
+  constructor(props){
+    super(props);
+    this.state = {
+      jokes : [],
+      message : 'Loading...'
+    }
+  }
+  
+  render() {
+      return (
         <div className='main'>
-
-          <div className='master-card'>
-            <div className='joke-stats'>
-              <div>1230</div>
-              <div id='hah'>HAH!</div>
-              <div id='nah'>nah.</div>
-              <div>43</div>
-            </div>
-            <div className='joke'>
-              <p>Setup : ________</p>
-              <p>Punchline: _______</p>
-            </div>
-          </div>
-          <div className='master-card'>
-            <div className='joke-stats'>
-              <div>1230</div>
-              <div id='hah'>HAH!</div>
-              <div id='nah'>nah.</div>
-              <div>43</div>
-            </div>
-            <div className='joke'>
-              <p>Setup : ________</p>
-              <p>Punchline: _______</p>
-            </div>
-          </div>
-          <div className='master-card'>
-            <div className='joke-stats'>
-              <div>1230</div>
-              <div id='hah'>HAH!</div>
-              <div id='nah'>nah.</div>
-              <div>43</div>
-            </div>
-            <div className='joke'>
-              <p>Setup : ________</p>
-              <p>Punchline: _______</p>
-            </div>
-          </div>
-          <div className='master-card'>
-            <div className='joke-stats'>
-              <div>1230</div>
-              <div id='hah'>HAH!</div>
-              <div id='nah'>nah.</div>
-              <div>43</div>
-            </div>
-            <div className='joke'>
-              <p>Setup : ________</p>
-              <p>Punchline: _______</p>
-            </div>
-          </div>
-          <div className='master-card last'>
-            <div className='joke-stats'>
-              <div>1230</div>
-              <div id='hah'>HAH!</div>
-              <div id='nah'>nah.</div>
-              <div>43</div>
-            </div>
-            <div className='joke'>
-              <p>Setup : ________</p>
-              <p>Punchline: _______</p>
-            </div>
-          </div>
+          {this.state.jokes.map((joke) => {
+            return <JokeCard username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
+          })}
+          <div style={{display:this.state.message ? 'block' : 'none'}}>{this.state.message}</div> 
         </div>
     );
+  }
+
+  componentDidMount(){
+    fetch('http://localhost/4001/jokes')
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        jokes : data['jokes'],
+        message : ''
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      this.setState({
+        message : 'Error loading jokes!'
+      })
+    });
+  }
 }
 
 export default JokeList;
