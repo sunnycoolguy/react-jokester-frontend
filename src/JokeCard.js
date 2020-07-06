329e95b2c544f9db3a85c8bbc810cd0ed7a5f0e4
senaybee <senay_berihu@outlook.com>
2020-07-06 15:51:05 -0700

    Implemet logic for deleting a joke

--- src/JokeCard.js
+++ src/JokeCard.js
@@ -2,13 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 class JokeCard extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            vote : ''
-        }
-    }
-
 
     render(){
         return (
@@ -21,7 +14,7 @@ class JokeCard extends React.Component {
                     <p className='punchline'>{`Punchline: ${this.props.punchline}` }</p>
                     <div className='joke-actions'>
                         {this.props.isCurrentUserJoke ? <Link className='edit-button' to={`/jokes/edit-a-joke/${this.props.id}`}>Edit</Link> : null}
-                        {this.props.isCurrentUserJoke ? <Link className='delete-button'>Delete</Link> : null}
+                        {this.props.isCurrentUserJoke ? <Link className='delete-button' onClick={this.props.onDelete} to={`/jokes/${this.props.username}`}>Delete</Link> : null}
                     </div>
                 </div>
             </div>
--- src/JokeListSingle.js
+++ src/JokeListSingle.js
@@ -8,6 +8,27 @@ class JokeListSingle extends React.Component {
             jokes : [],
             message : 'Loading...',
         }
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    
+    handleDelete(id){
+        fetch(`http://localhost:4001/jokes/${this.props.currentUser}/${id}`, {
+            method : 'DELETE',
+        })
+        .then((response) => {
+            if(!response.ok){
+                throw Error();
+            }
+            return response
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('hello', data)
+            this.setState({jokes : data})
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
 
     render() {
@@ -17,15 +38,15 @@ class JokeListSingle extends React.Component {
             <div style={{fontSize : '23px', marginLeft: '16px'}}>{`${this.props.match.params.username}'s jokes:`}</div>
             {this.state.jokes.map((joke, index) => {
               if(index=== 0 && index === this.state.jokes.length - 1){
-                return <JokeCard id={joke['_id']} isCurrentUserJoke={isCurrentUserJoke} match={this.props.match} likedBy={joke.likedBy} dislikedBy={joke.dislikedBy} key={joke['_id']} isFirst={true} isLast={true} username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
+                return <JokeCard onDelete={()=> this.handleDelete(joke['_id'])} id={joke['_id']} isCurrentUserJoke={isCurrentUserJoke} match={this.props.match} likedBy={joke.likedBy} dislikedBy={joke.dislikedBy} key={joke['_id']} isFirst={true} isLast={true} username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
               }
               if(index === 0){
-                return <JokeCard id={joke['_id']} isCurrentUserJoke={isCurrentUserJoke} match={this.props.match} likedBy={joke.likedBy} dislikedBy={joke.dislikedBy}  key={joke['_id']} isFirst={true} username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
+                return <JokeCard onDelete={()=> this.handleDelete(joke['_id'])} id={joke['_id']} isCurrentUserJoke={isCurrentUserJoke} match={this.props.match} likedBy={joke.likedBy} dislikedBy={joke.dislikedBy}  key={joke['_id']} isFirst={true} username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
               }
               if(index === this.state.jokes.length - 1){
-                return <JokeCard id={joke['_id']} isCurrentUserJoke={isCurrentUserJoke} match={this.props.match} likedBy={joke.likedBy} dislikedBy={joke.dislikedBy}  key={joke['_id']} isLast={true} username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
+                return <JokeCard onDelete={()=> this.handleDelete(joke['_id'])} id={joke['_id']} isCurrentUserJoke={isCurrentUserJoke} match={this.props.match} likedBy={joke.likedBy} dislikedBy={joke.dislikedBy}  key={joke['_id']} isLast={true} username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
               }
-              return <JokeCard id={joke['_id']} isCurrentUserJoke={isCurrentUserJoke} match={this.props.match} likedBy={joke.likedBy} dislikedBy={joke.dislikedBy} key={joke['_id']} username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
+              return <JokeCard onDelete={()=> this.handleDelete(joke['_id'])} id={joke['_id']} isCurrentUserJoke={isCurrentUserJoke} match={this.props.match} likedBy={joke.likedBy} dislikedBy={joke.dislikedBy} key={joke['_id']} username={joke.username} setup={joke.setup} punchline={joke.punchline}/>
             })}
             <div style={{display:this.state.message ? 'block' : 'none'}}>{this.state.message}</div> 
           </div>
